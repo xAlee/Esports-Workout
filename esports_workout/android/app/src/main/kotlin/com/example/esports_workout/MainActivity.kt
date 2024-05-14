afc63f5ed1af1e0d0b69013390ca12bfdc18ea14
Alejandro Gutierrez <alejandroxchile@gmail.com>
2024-05-13 20:57:44 -0400

    Base de Proyecto

---
+++ esports_workout/android/app/src/main/kotlin/com/example/esports_workout/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.esports_workout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ esports_workout/lib/data/User.dart
@@ -0,0 +1,7 @@
+class User {
+  final String name;
+  final String email;
+  final DateTime lastAccess;
+
+  User(this.name, this.email, this.lastAccess);
+}
